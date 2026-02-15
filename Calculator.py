035f4a099df782119300482add813cb7a967d4fe
neerajpainuly2007-ux <neerajpainuly2007@gmail.com>
2026-02-15 15:52:16 +0530

    Add simple calculator functionality in Calculator.py
    Python-based calculator project performing addition, subtraction, multiplication, and division.

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+#  Calculator Program 
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+# User input
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Calculation...
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2 
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Division by zero is not allowed!")
+
+else:
+    print("Invalid operator!")
